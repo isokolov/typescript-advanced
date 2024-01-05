e09002e24b4d7a840807d3096c1a2fe8946eef51
isokolov <illya.sokolov82@gmail.com>
2024-01-05 10:39:02 +0200

    refactor with abstract class, Generics

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,18 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
 
-const readCSV = new CsvFileReader('football.csv');
+const readCSV = new MatchReader('football.csv');
 readCSV.read();
 
 console.log(readCSV.data);
+let manUnitedWins = 0;
+
+for (let match of readCSV.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
